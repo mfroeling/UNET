2c7e0dab9d6f2ec53a0522ba959aae9a1b370c2a
mfroelin <mfroelin@LA022122.ds.umcutrecht.nl>
2021-08-16 14:08:56 +0200

    build 12.3

---
+++ build/UNET/PacletInfo.m
@@ -0,0 +1,21 @@
+(* Paclet Info File *)
+
+(* created 2018/12/14*)
+
+Paclet[
+    Name -> "UNET",
+    Version -> "1.1.0",
+    WolframVersion -> "12.3+",
+    Description -> "A package to generate and train a UNET deep convolutional network for 2D and 3D image segmentation",
+    Creator -> "Martijn Froeling <m.froeling@gmail.com>",
+    Support -> "https://github.com/mfroeling/UNET",
+    Icon -> "thumb.gif",
+    Loading -> "Manual",
+    Extensions -> 
+        {
+            {"Kernel", Root -> "Kernel", Context -> "UNET`"},
+            {"Documentation", Language -> "English", MainPage -> "Guides/UNET"}
+        }
+]
+
+
